73c8efa529c37c32c0d1e7567cef100ce17a5474
Chih-Yung Liang <cyliang@users.noreply.github.com>
2014-12-16 19:34:20 +0800

    Update tone.c

--- tone.c
+++ tone.c
@@ -7,19 +7,20 @@ void tone_init() {
 	T2MOD = 0x02;
 
 	/* Set default tone to A4 */
-	tone_set(TONE_A, 4);
+	tone_set(TONE(TONE_A, 4));
 }
 
 void tone_onoff(bit on_off) {
 	TR2 = on_off;
 }
 
-void tone_set(unsigned short tone, unsigned char offset) {
+void tone_set(unsigned short tone) {
 	unsigned short period_m;
 
-	tone >> (7-offset + 1);
-	period_m = 1000000 / tone;
+	period_m = 3000000 / tone;
 
-	TH2 = RCAP2H = (65536 - period_m) >> 8;
-	TL2 = RCAP2L = (65536 - period_m) & 0x00FF;
+	TH2 = 0;
+	TL2 = 0;
+	RCAP2H = (65536 - period_m) >> 8;
+	RCAP2L = (65536 - period_m) & 0x00FF;
 }
